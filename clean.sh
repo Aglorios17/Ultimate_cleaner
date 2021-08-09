2298e007ea080620cd7ac5885d9d48ecb79ba2fb
Alessio Glorioso <56506324+Aglorios17@users.noreply.github.com>
2021-08-09 16:26:40 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -4,7 +4,7 @@ VALUE=0;
 brew install switchaudio-osx
 SwitchAudioSource -t output -s 'Built-in Output'
 
-osascript -e "set Volume 10"
+#osascript -e "set Volume 10"
 
 curl https://img.over-blog-kiwi.com/0/93/14/83/20180912/ob_0f4f4d_serial-cleaner.jpg > $HOME/mr_propre.jpg
 
