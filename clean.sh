13ce6170208d7e6c5925bb4183c90c9238e3b0a3
Alessio Glorioso <56506324+Aglorios17@users.noreply.github.com>
2021-08-10 16:12:21 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -4,7 +4,7 @@ VALUE=0;
 brew install switchaudio-osx
 SwitchAudioSource -t output -s 'Built-in Output'
 
-#osascript -e "set Volume 10"
+osascript -e "set Volume 10"
 
 curl https://img.over-blog-kiwi.com/0/93/14/83/20180912/ob_0f4f4d_serial-cleaner.jpg > $HOME/mr_propre.jpg
 
@@ -14,7 +14,7 @@ open -a Safari https://www.youtube.com/watch?v=mNyDfkDsSP4
 while ((VALUE!=10))
 	do	
 		open ~/mr_propre.jpg
-#		touch ~/desktop/cleaning_$VALUE
+		touch ~/desktop/cleaning_$VALUE
 		((VALUE+=1))
 done
 rm -rf ~/cleaner
